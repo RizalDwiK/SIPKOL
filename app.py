c73de8a84f7aa523aaa9632a992982afaeaa7903
RizalDwiK <147721563+RizalDwiK@users.noreply.github.com>
2026-07-09 00:49:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ with right:
 
 if predict:
 
-    API_URL = "https://sipkolbreastcancer-x3i7n28e.b4a.run/predict"
+    API_URL = "https://sipkolbreastcancer-va8q8xpl.b4a.run//predict"
 
     try:
 
